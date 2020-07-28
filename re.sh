f8a0288919a7f74e3f073d4fa8d0be99b2f577a7
Kieran Milligan <kieran.milligan@gmail.com>
2020-07-28 16:13:33 -0500

    created features/arguments

--- re.sh
+++ re.sh
@@ -4,9 +4,10 @@
 cd /etc/apache2/sites-available
 
 # Disable a vhost configuration
-sudo a2dissite *
-sudo service apache2 restart
+sudo a2dissite "$CONFIG"
+sudo service apache2 "$COMMAND"
 
 # Enable a vhost configuration
-sudo a2ensite *
-sudo service apache2 restart
+sudo a2ensite "$CONFIG"
+sudo service apache2 "$COMMAND"
+
