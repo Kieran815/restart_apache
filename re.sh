df526c7b276cf81546259daad857ce4a0490175c
Kieran Milligan <kieran.milligan@gmail.com>
2020-07-28 16:37:30 -0500

    updated bash shell

--- re.sh
+++ re.sh
@@ -1,13 +1,21 @@
 #!/bin/bash
+CONFIG="$1"
+COMMAND="$2"
 
-# Move the current execution state to the proper directory
-cd /etc/apache2/sites-available
+# reload is allowed
+if [ "$COMMAND" == "reload" ] || [ "$COMMAND" == "restart" ]
+then
+    # Move the current execution state to the proper directory
+    cd /etc/apache2/sites-available
 
-# Disable a vhost configuration
-sudo a2dissite "$CONFIG"
-sudo service apache2 "$COMMAND"
+    # Disable a vhost configuration
+    sudo a2dissite "$CONFIG"
+    sudo service apache2 "$COMMAND"
 
-# Enable a vhost configuration
-sudo a2ensite "$CONFIG"
-sudo service apache2 "$COMMAND"
-
+    # Enable a vhost configuration
+    sudo a2ensite "$CONFIG"
+    sudo service apache2 "$COMMAND"
+else
+    echo "ERROR: $COMMAND is an invalid service command {restart|reload}"
+    exit 1
+fi
