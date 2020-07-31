b4d19c1b4f516a3ce07c0116d57bf5a409a71d01
Kieran Milligan <kieran.milligan@gmail.com>
2020-07-31 11:00:24 -0500

    moved .sh example files into repo

--- re.sh
+++ re.sh
@@ -3,21 +3,39 @@
 CONFIG="$1"
 COMMAND="$2"
 
+VHOSTS_PATH=/etc/apache2/sites-available/*.conf
+
+
 # only allow reload or restart.
 if [ "$COMMAND" == "reload" ] || [ "$COMMAND" == "restart" ]
 then
-    # Move the current execution state to the proper directory
-    cd /etc/apache2/sites-available
+    if ["$CONFIG" === "$VHOST_PATH[@]"]
+    then
+        # Move the current execution state to the proper directory
+        cd /etc/apache2/sites-available
 
-    # Disable a vhost configuration
-    sudo a2dissite "$CONFIG"
-    sudo service apache2 "$COMMAND"
+        # Disable a vhost configuration
+        sudo a2dissite "$CONFIG"
+        sudo service apache2 "$COMMAND"
 
-    # Enable a vhost configuration
-    sudo a2ensite "$CONFIG"
-    sudo service apache2 "$COMMAND"
+        # Enable a vhost configuration
+        sudo a2ensite "$CONFIG"
+        sudo service apache2 "$COMMAND"
+    else
+        echo "ERROR: "$CONFIG" is not a working config file. Please select from the following:"
+        for FILENAME in $VHOSTS_PATH
+        do
+        echo $FILENAME
+        done
+        exit 1
+    fi
 else
-    echo "ERROR: $COMMAND is an invalid service command {restart|reload}"
+    echo "ERROR: $0 requires two paramters {virtual-host} {restart|reload}"
     exit 1
 fi
 
+# Update the re.sh script such that
+
+# The user will receive an error message if they attempt to reload/restart a virtual-host file that does not exist in /etc/apache2/sites-available.
+# The system will return a list of valid files as a part of the error message.
+# The system will exit prior to evaluating the reload command if an invalid virtual-host has been chosen.
